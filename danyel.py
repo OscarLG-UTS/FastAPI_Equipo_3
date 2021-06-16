ae870abe971113e7b3f4c628f384cec5c1a3ec9f
Daniel-Cruz85 <josedanielcrz64@gmail.com>
2021-06-15 21:38:08 -0500

    Update danyel.py
    dani

--- danyel.py
+++ danyel.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from typing import Optional 
+
 app = FastAPI()
+
 @app.get("/")
-async def root():
-    return {"message": "ejercicio daniel"}
-    
+def read_root():
+    return {"ejercicio daniel"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return{"item_id": item_id, "q":q}   
